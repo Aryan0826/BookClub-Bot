daa5829ed082e43378cc7068167c888576ea99b0
Aryan0826 <165449842+Aryan0826@users.noreply.github.com>
2025-04-26 15:08:13 -0500

    change from using client to commands

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
 load_dotenv()  # reads .env into os.environ
@@ -6,35 +7,25 @@ load_dotenv()  # reads .env into os.environ
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+intents.members = True
 
-# client =  discord.Client()
+bot = commands.Bot(command_prefix = "$", intents = intents)
+
+verificationCodes = {} # Store verification codes: user_id -> verification code
 
 #we are creating certain functions for discord bot to respond to 
 # these function names are actually from the discord.py library 
 # it is looking specifically for these function names
 
 # This is an event executed as soon as the bot is ready and working
-@client.event
+@bot.event
 async def on_ready(): 
-    print("We have logged in as {0.user}".format(client))
+    print("We have logged in as {bot.user}")
 
 
-#event triggered if it senses a message in the discord server
-# args: message
-@client.event
-async def on_message(message):
-# this on mesage eveent triggers each time a message is recieved
-# we dont want our bot to trigger it for its own messages
-    if message.author == client.user:
-        return
-    
-    # if message starts with a command thats being sent to our bot
-    # we can make up commads for our bot
-    # lets say for our bot all commands start with $
-    if message.content.startswith("$hello"):
-        # to return a message back to discord
-        await message.channel.send("Hello!")
+@bot.command(name = "hello")
+async def hello(ctx):
+    await ctx.send("Hello!")
 
 # to run the bot 
 # we need to pass the token to the run command
@@ -42,4 +33,4 @@ async def on_message(message):
 token = os.getenv("TOKEN")
 if not token:
     raise RuntimeError("Missing TOKEN environment variable")
-client.run(token)
+bot.run(token)
