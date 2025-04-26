1a1165de7f0471b7a537bf30ea2adab0f17a0dc1
Aryan0826 <165449842+Aryan0826@users.noreply.github.com>
2025-04-26 16:54:33 -0500

    Add welcome message with instructions to verify

--- main.py
+++ main.py
@@ -20,12 +20,20 @@ verificationCodes = {} # Store verification codes: user_id -> verification code
 # This is an event executed as soon as the bot is ready and working
 @bot.event
 async def on_ready(): 
-    print("We have logged in as {bot.user}")
+    print(f"We have logged in as {bot.user}")
 
+@bot.event
+async def on_member_join(member):
+    welcomeChannelID = int(os.getenv("WELCOME_CHANNEL_ID",0))
+    if welcomeChannelID:
+        welcomeChannel = bot.get_channel(welcomeChannelID)
+        if welcomeChannel:
+            await welcomeChannel.send(f"Welcome {member.mention}! To access the server, please verify yourself by typing "
+                f"`$verify` in this channel. I'll send you a direct message with instructions.")
 
 @bot.command(name = "hello")
 async def hello(ctx):
-    await ctx.send("Hello!")
+    await ctx.send("Hello!")    
 
 # to run the bot 
 # we need to pass the token to the run command
